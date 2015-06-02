6a46b8ea2b6838e20a293c27cdf83a2b9656753b
MPMSEB <cerezuela.seb@gmail.com>
2015-06-02 14:12:31 +0200

    Initial Commit

---
+++ MacleanRH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MacleanRH
+//
+//  Created by iem on 02/06/2015.
+//  Copyright (c) 2015 iem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
