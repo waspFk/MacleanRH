33bb747f7925186142f1e7e6c31465ee1fe5df02
Fonsat Nicolas <fonsat.nicolas@gmail.com>
2015-06-08 10:25:05 +0200

    add accesseur for NSSet Candidate

--- MacleanRH/Candidate.swift
+++ MacleanRH/Candidate.swift
@@ -28,9 +28,37 @@ class Candidate: NSManagedObject {
 
 extension Candidate {
     
-    func addRecruitment(recruitment:Recruitment) {
+    func addRecruitment(recruitment: Recruitment) {
         var recruitments = self.mutableSetValueForKey("recruitments")
         recruitments.addObject(recruitment)
     }
     
+    func countRecruitments() -> Int {
+        return self.recruitments.count
+    }
+    
+    func getRecruitmens() -> [Recruitment] {
+        var tmpRecruitments = [Recruitment]()
+        
+        tmpRecruitments = self.recruitments.allObjects as! [Recruitment]
+        
+        return tmpRecruitments
+    }
+    
+    func addDegree(degree: Degree) {
+        var degrees = self.mutableSetValueForKey("degrees")
+        degrees.addObject(degree)
+    }
+    
+    func countDegrees() -> Int {
+        return self.degrees.count
+    }
+    
+    func getDegrees() -> [Degree] {
+        var tmpDegrees = [Degree]()
+        
+        tmpDegrees = self.degrees.allObjects as! [Degree]
+        
+        return tmpDegrees
+    }
 }
