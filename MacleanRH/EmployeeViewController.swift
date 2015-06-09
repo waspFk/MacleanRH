78d14455fca81ff9c5be0d7b2cad1047a14ec303
azd aze <macleanrh@yopmail.com>
2015-06-09 10:53:05 +0200

    debut mise en pace detail employee

--- MacleanRH/EmployeeViewController.swift
+++ MacleanRH/EmployeeViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class EmployeeViewController: UIViewController {
     
-  
+    var employee : Employee!
     
+    @IBOutlet weak var photoEmployee: UIImageView!
+    @IBOutlet weak var lastNameEmployee: UITextField!
+    @IBOutlet weak var firstNameEmployee: UITextField!
+    @IBOutlet weak var adresseEmployee: UITextField!
+    @IBOutlet weak var mailEmployee: UITextField!
+    @IBOutlet weak var phoneEmployee: UITextField!
+    @IBOutlet weak var mobileEmployee: UITextField!
+    @IBOutlet weak var secteurEmployee: UIPickerView!
+    @IBOutlet weak var posteEmloyee: UIPickerView!
+    @IBOutlet weak var contratEmployee: UISegmentedControl!
+    @IBOutlet weak var beginDateContrat: UITextField!
+    @IBOutlet weak var endDateContrat: UITextField!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        println(employee.description)
+        
+        firstNameEmployee.text = 
+    }
     
     @IBAction func getWebViewWithViadeo(sender: AnyObject) {
         let viewController = self.storyboard?.instantiateViewControllerWithIdentifier("WebViewController") as! WebViewController
@@ -30,9 +49,5 @@ class EmployeeViewController: UIViewController {
         let viewController = self.storyboard?.instantiateViewControllerWithIdentifier("WebViewController") as! WebViewController
         viewController.url = "https://www.xing.com/fr"
         self.navigationController?.pushViewController(viewController, animated: true)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
     }
 }
--- MacleanRH/ListingEmployeeViewController.swift
+++ MacleanRH/ListingEmployeeViewController.swift
@@ -42,4 +42,14 @@ class ListingEmployeeViewController: UIViewController, UITableViewDelegate, UITa
         return cell
     }
     
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if segue.identifier == "detailEmployee" {
+            if let destination = segue.destinationViewController as? EmployeeViewController {
+                if let index = tableEmployees.indexPathForSelectedRow()?.row {
+                    destination.employee = employees[index]
+                }
+            }
+        }
+    }
+    
 }
