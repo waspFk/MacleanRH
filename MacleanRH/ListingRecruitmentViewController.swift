852f62c0ba2e88822f228339623789407155d124
Fonsat Nicolas <fonsat.nicolas@gmail.com>
2015-06-10 14:29:07 +0200

    add search

--- MacleanRH/ListingRecruitmentViewController.swift
+++ MacleanRH/ListingRecruitmentViewController.swift
@@ -8,34 +8,101 @@
 
 import UIKit
 
-class ListingRecruitmentViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ListingRecruitmentViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     var recruitments:[Recruitment]!
+    var filteredTableRecuitment:[Recruitment]!
+    var searchBarActive :Bool!
     
+    
+    @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        searchBarActive = false
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
+        
+        filteredTableRecuitment = [Recruitment]()
+        
         loadData()
     }
     
+    
+    // MARK: - SearchBar
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        println(" --searchBarTextDidBeginEditing")
+        searchBarActive = true
+    }
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        println(" --searchBarTextDidEndEditing")
+        searchBarActive = false;
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        println(" --searchBarCancelButtonClicked")
+        searchBarActive = false;
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        println(" --searchBarSearchButtonClicked")
+        searchBarActive = false;
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        println(" --textDidChange")
+        
+        self.filteredTableRecuitment = self.recruitments.filter(
+            {
+                ( recruitment: Recruitment) -> Bool in
+                println("SearhText = \(searchText)")
+                let titleMatch = recruitment.titre.rangeOfString(searchText)
+                let workLibelleMatch = recruitment.workLibelle.rangeOfString(searchText)
+                
+                
+                println("Title = \(titleMatch)")
+                println("Work = \(workLibelleMatch)")
+                
+                return (titleMatch != nil) || (workLibelleMatch != nil)
+            }
+        )
+        
+        if(filteredTableRecuitment.count == 0){
+            searchBarActive = false;
+        } else {
+            searchBarActive = true;
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    
     // MARK: - Helper UI
     func loadData() {
-        println(" --loadData")
         recruitments = RecruitmentManager.SharedManager.getAllRecruitments(nil)
-        
-        println(" --loadData : \(recruitments.count)")
     }
     
     
     // MARK: - UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        println(" --numberOfRowsInSection : \(recruitments.count) ")
+        if(searchBarActive  == true){
+            return filteredTableRecuitment.count
+        }
         return recruitments.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = self.tableView.dequeueReusableCellWithIdentifier("RecruitmentCell") as! RecruitmentViewCell
-        let recruitment = recruitments[indexPath.row]
+        var recruitment:Recruitment!
+        
+        if(searchBarActive  == true){
+            recruitment = filteredTableRecuitment[indexPath.row]
+        } else {
+            recruitment = recruitments[indexPath.row]
+        }
         
         cell.posteLabel.text = recruitment.titre
         cell.descriptionPosteLabel.text = recruitment.workDescription
